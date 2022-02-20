513bc6ff00205a0f7cbc01e985e8e4812f5c84eb
mohammad badri <mohammad.b@arianalabs.com>
2022-02-20 21:46:22 +0330

    add api get

--- index.js
+++ index.js
@@ -4,6 +4,45 @@ const app = express();
 
 app.use(express.json());
 
+let products = [
+  {
+    id: 1,
+    name: "pen",
+    price: 3000,
+    color: "blue",
+    count: 10,
+  },
+  {
+    id: 2,
+    name: "shoe",
+    price: 80000,
+    color: "brown",
+    count: 10,
+  },
+  {
+    id: 3,
+    name: "pencil",
+    price: 1000,
+    color: "red",
+    count: 10,
+  },
+];
+
+// get list
+app.get("/api/products", (req, res) => {
+  res.send(products);
+});
+
+// get per id
+app.get("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = products.find((product) => product.id == id);
+
+  if (!product) res.status(404).send({});
+
+  res.send(product);
+});
+
 // port
 const port = process.env.port || 3000;
 
