20fa447b243cc8c0a04278683fe60ea894b69630
MrBadri <badri0000badri@gmail.com>
2022-03-31 21:28:42 +0430

    add base functions validators products

---
+++ validators/product/index.js
@@ -0,0 +1,33 @@
+const joi = require("joi");
+joi.objectId = require("joi-objectid")(joi);
+
+const validateCreateCustomer = (data) => {
+  const schema = joi.object({});
+
+  return schema.validate(data);
+};
+
+const validateReadCustomer = (data) => {
+  const schema = joi.object({});
+
+  return schema.validate(data);
+};
+
+const validateUpdateCustomer = (data) => {
+  const schema = joi.object({});
+
+  return schema.validate(data);
+};
+
+const validateDeleteCustomer = (data) => {
+  const schema = joi.object({});
+
+  return schema.validate(data);
+};
+
+modile.exports = {
+  validateCreateCustomer,
+  validateReadCustomer,
+  validateUpdateCustomer,
+  validateDeleteCustomer,
+};
